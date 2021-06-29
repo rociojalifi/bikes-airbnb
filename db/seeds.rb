22e01e11827120f23cdda4684d358c5d00f986c2
Eugenia Amaya <eugenia@estudioamaya.net>
2021-06-29 14:41:26 -0300

    showww

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bike1 = Bike.create(photo:"https://i.blogs.es/a2d4ed/bicicleta-porsche-5/450_1000.jpg", description: "Model: Star Trek 3000. Color: Blue", price: 3000, location: "Terrada 4743, Luján, Mendoza, Argentina")
